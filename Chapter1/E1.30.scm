e739f6949e9338fa21478b26ea607ddc23452eb3
Shivram Khandeparker <shivramk@gmail.com>
2014-08-05 23:53:34 +0530

    Added solutions for exercises 1.30 and 1.31

---
+++ Chapter1/E1.30.scm
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+; iterative version of sum
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (integral f a b dx)
+  (define (add-dx x)
+    (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
+
+(define (cube x) (* x x x))
+
+(integral cube 0 1 0.001) ; 0.249999875000001
