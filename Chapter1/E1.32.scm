d2f6add5f274004af3c3aa627369b00d9e57fbd9
Shivram Khandeparker <shivramk@gmail.com>
2014-08-06 00:03:05 +0530

    Corrected accidental commit with correct contents

--- Chapter1/E1.32.scm
+++ Chapter1/E1.32.scm
@@ -1,22 +1,19 @@
 #lang planet neil/sicp
 
-; iterative version of product
-(define (product term a next b)
+; iterative version of accumulate
+(define (accumulate combiner null-value term a next b)
   (define (iter a result)
     (if (> a b)
         result
-        (iter (next a) (* result (term a)))))
-  (iter a 1))
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+; a. implement sum and product using accumulate
+(define (sum term a next b) (accumulate + 0 term a next b))
+(define (product term a next b) (accumulate * 1 term a next b))
 
 (define (id x) x)
 
-; integer division
-(define (idiv x n)
-  (if (= (remainder x n) 0)
-      (/ x n)
-      (- (/ x n) (/ (remainder x n) n))))
-
-; a. factorial + pi
 ; factorial implementation
 (define (factorial n)
   (product id 1 inc n))
@@ -24,27 +21,33 @@
 (factorial 5) ; 120
 (factorial 10) ; 3628800
 
-(define (feven n)
-  (+ 2 (* (idiv n 2) 2)))
-(define (fodd n)
-  (+ 3 (* (idiv n 2) 2)))
+; sum 1 to n
+(define (sum-n n)
+  (sum id 1 inc n))
 
-; compute the value of PI
-(define (compute-pi)
-  (* 4.0 (/ (product feven 1 inc 10000) 
-            (product fodd 0 inc 9999))))
+(sum-n 10) ; 55
+(sum-n 100) ; 5050
 
-(compute-pi) ; 3.1417497057380523
-
-; b. recursive version of product
-(define (product2 term a next b)
+; b. recursive version of accumulate
+(define (accumulate2 combiner null-value term a next b)
   (if (> a b)
-      1
-      (* (term a)
-         (product2 term (next a) next b))))
+      null-value
+      (combiner (term a)
+         (accumulate2 combiner null-value term (next a) next b))))
 
+(define (sum2 term a next b) (accumulate2 + 0 term a next b))
+(define (product2 term a next b) (accumulate2 * 1 term a next b))
+
+; factorial and sum. should give same results as before
 (define (factorial2 n)
   (product2 id 1 inc n))
 
-(factorial 5) ; 120
-(factorial 10) ; 3628800
+(factorial2 5) ; 120
+(factorial2 10) ; 3628800
+
+; sum 1 to n
+(define (sum-n2 n)
+  (sum2 id 1 inc n))
+
+(sum-n2 10) ; 55
+(sum-n2 100) ; 5050
