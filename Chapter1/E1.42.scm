3b381829b082b99bbf189b620567f4cffe79af75
Shivram Khandeparker <shivramk@gmail.com>
2014-08-06 22:36:58 +0530

    Added solutions to exercises 1.42 and 1.43

---
+++ Chapter1/E1.42.scm
@@ -0,0 +1,8 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+
+(define (compose f g) 
+  (lambda (x) (f (g x))))
+
+((compose square inc) 6) ; 49
---
+++ Chapter1/E1.43.scm
@@ -0,0 +1,16 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (define (repeat i)
+    (if (= i 1) 
+      f
+      (compose f (repeat (- i 1)))))
+  (repeat n))
+
+((repeated square 2) 5) ; 625
+((repeated inc 5) 5) ; 10
