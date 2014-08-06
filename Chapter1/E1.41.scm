e5190aa2c0c053aaa3cd3f48ede0aefd3984b55e
Shivram Khandeparker <shivramk@gmail.com>
2014-08-06 22:18:32 +0530

    Added solutions to exercises 1.40 and 1.41

---
+++ Chapter1/E1.41.scm
@@ -0,0 +1,20 @@
+#lang planet neil/sicp
+
+(define (double f)
+  (lambda (x) (f (f x))))
+
+(((double (double double)) inc) 5) ; gives 21
+
+; why is this so? Let us abrreviate double as d
+;
+; consider the innermost expression (double double)
+; (d d) -> (lambda (x) (d (d x)))
+;
+; Now consider (double (double double))
+; (d (d d)) -> (lambda (x) (d (d (d (d x)))))
+;
+; This makes ((double (double double)) inc)
+; ((d (d d)) inc) -> (lambda (x) (d (d (d (d (inc x))))))
+; 
+; That is equivalent to 4 doublings or 2^4 = 16
+; 16 + 5 = 21
