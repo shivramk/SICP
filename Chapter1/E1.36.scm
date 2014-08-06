d96b92d07ee4d10681805e0ba4f52d52d53896b0
Shivram Khandeparker <shivramk@gmail.com>
2014-08-06 16:03:49 +0530

    Added solutions to exercises 1.36 and 1.37

---
+++ Chapter1/E1.36.scm
@@ -0,0 +1,26 @@
+#lang planet neil/sicp
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average x y) (/ (+ x y) 2))
+
+; takes 8 steps -> 4.55553609061889
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 3)
+
+; takes 32 steps -> 4.555532257016376
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 3)
+
+; Average damping takes fewer steps
---
+++ Chapter1/E1.37.scm
@@ -0,0 +1,28 @@
+#lang planet neil/sicp
+
+; recursive version of cont-frac
+(define (cont-frac n d k)
+  (define (compute-cont-frac i)
+    (if (>= i k) 
+        (/ (n i) (d i))
+        (/ (n i) (+ (d i) (compute-cont-frac (+ i 1))))))
+  (compute-cont-frac 1))
+
+; phi
+(/ (+ 1 (sqrt 5)) 2)
+
+; a. approximate value of phi using cont-frac
+; k = 12 is sufficient to get 4 decimal places of accuracy
+(/ 1 (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 12))
+
+; b. iterative version of cont-frac 
+; Start from the lowermost n/d and work our way upwards
+(define (cont-frac2 n d k)
+  (define (cont-frac-iter i s)
+    (if (= i 0)
+        s
+        (cont-frac-iter (- i 1) (/ (n i) (+ (d i) s)))))
+  (cont-frac-iter k 0))
+
+; should be same as above
+(/ 1 (cont-frac2 (lambda (i) 1.0) (lambda (i) 1.0) 12))
