854d285dfb5d99abed75b204da836aa279a65091
Shivram Khandeparker <shivramk@gmail.com>
2014-08-09 21:07:38 +0530

    Added solution for 2.4

---
+++ Chapter2/E2.4.scm
@@ -0,0 +1,11 @@
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 10 20)); 10
+(cdr (cons 10 20)); 20
