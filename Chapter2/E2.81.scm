f0e8e785a023d92ff8df2bf811e10528a5f9d3eb
Shivram Khandeparker <shivramk@gmail.com>
2015-02-24 00:14:39 +0530

    Refactored 2.81 to use code from 2.79

--- Chapter2/E2.81.scm
+++ Chapter2/E2.81.scm
@@ -1,169 +1,6 @@
-#lang planet neil/sicp
+(include "E2.80.scm")
 
-;; Support for put and get (provided by SICP authors in ch2support.scm)
-;; http://mitpress.mit.edu/sicp/code/ch2support.scm
-(define (assoc key records)
-  (cond ((null? records) false)
-        ((equal? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
-
-(define (make-table)
-  (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
-            false)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table)))))
-      'ok)    
-    (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            (else (error "Unknown operation -- TABLE" m))))
-    dispatch))
-
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
-
-(define (attach-tag type-tag contents)
-  (if (eq? type-tag 'scheme-number)
-      contents
-      (cons type-tag contents)))
-
-(define (type-tag datum)
-  (cond ((pair? datum) (car datum))
-        ((number? datum) 'scheme-number)
-        (else (error "Bad tagged datum -- TYPE-TAG" datum))))
-
-(define (contents datum)
-  (cond ((pair? datum) (cdr datum))
-        ((number? datum) datum)
-        (else (error "Bad tagged datum -- CONTENTS" datum))))
-
-(define (square x) (* x x))
-
-(define (install-rectangular-package)
-  ;; internal procedures
-  (define (real-part z) (car z))
-  (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) (cons x y))
-  (define (magnitude z)
-    (sqrt (+ (square (real-part z))
-             (square (imag-part z)))))
-  (define (angle z)
-    (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a) 
-    (cons (* r (cos a)) (* r (sin a))))
-
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'rectangular x))
-  (put 'real-part '(rectangular) real-part)
-  (put 'imag-part '(rectangular) imag-part)
-  (put 'magnitude '(rectangular) magnitude)
-  (put 'angle '(rectangular) angle)
-  (put 'make-from-real-imag 'rectangular
-       (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'rectangular
-       (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-(define (install-polar-package)
-  ;; internal procedures
-  (define (magnitude z) (car z))
-  (define (angle z) (cdr z))
-  (define (make-from-mag-ang r a) (cons r a))
-  (define (real-part z)
-    (* (magnitude z) (cos (angle z))))
-  (define (imag-part z)
-    (* (magnitude z) (sin (angle z))))
-  (define (make-from-real-imag x y) 
-    (cons (sqrt (+ (square x) (square y)))
-          (atan y x)))
-
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag 'polar x))
-  (put 'real-part '(polar) real-part)
-  (put 'imag-part '(polar) imag-part)
-  (put 'magnitude '(polar) magnitude)
-  (put 'angle '(polar) angle)
-  (put 'make-from-real-imag 'polar
-       (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'polar
-       (lambda (r a) (tag (make-from-mag-ang r a))))
-  'done)
-
-(define (apply-generic_ op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags))) 
-      (if proc
-          (apply proc (map contents args)) 
-          (if (= (length args) 2)
-              (let ((type1 (car type-tags)) 
-                    (type2 (cadr type-tags)) 
-                    (a1 (car args))
-                    (a2 (cadr args)))
-                (let ((t1->t2 (get-coercion type1 type2))
-                      (t2->t1 (get-coercion type2 type1)))
-                  (cond (t1->t2
-                         (apply-generic op (t1->t2 a1) a2))
-                        (t2->t1
-                         (apply-generic op a1 (t2->t1 a2)))
-                        (else (error "No method for these types" (list op type-tags))))))
-              (error "No method for these types"
-                     (list op type-tags)))))))
-
-
-;; C. modified apply-generic
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags))) 
-      (if proc
-          (apply proc (map contents args)) 
-          (if (= (length args) 2)
-              (let ((type1 (car type-tags)) 
-                    (type2 (cadr type-tags)))
-                (if (eq? type1 type2)
-                    (error "Not method for these types" (list op type-tags))
-                    (let ((t1->t2 (get-coercion type1 type2))
-                          (t2->t1 (get-coercion type2 type1))
-                           (a1 (car args))
-                           (a2 (cadr args)))
-                      (cond (t1->t2
-                             (apply-generic op (t1->t2 a1) a2))
-                            (t2->t1
-                             (apply-generic op a1 (t2->t1 a2)))
-                            (else (error "No method for these types" (list op type-tags)))))))
-                (error "No method for these types"
-                       (list op type-tags)))))))
-
-(define (real-part z) (apply-generic 'real-part z))
-(define (imag-part z) (apply-generic 'imag-part z))
-(define (magnitude z) 
-  (apply-generic 'magnitude z))
-(define (angle z) (apply-generic 'angle z))
-
-(define (add x y) (apply-generic 'add x y))
-(define (sub x y) (apply-generic 'sub x y))
-(define (mul x y) (apply-generic 'mul x y))
-(define (div x y) (apply-generic 'div x y))
-(define (equ? x y) (apply-generic 'equ? x y))
-(define (=zero? x) (apply-generic '=zero? x))
+(define (exp x y) (apply-generic 'exp x y))
 
 (define (install-scheme-number-package)
   (define (tag x)
@@ -186,109 +23,6 @@
        (lambda (x y) (tag (expt x y))))
   'done)
 
-(define (make-scheme-number n)
-  ((get 'make 'scheme-number) n))
-
-(define (install-rational-package)
-  ;; internal procedures
-  (define (numer x) (car x))
-  (define (denom x) (cdr x))
-  (define (make-rat n d)
-    (let ((g (gcd n d)))
-      (cons (/ n g) (/ d g))))
-  (define (add-rat x y)
-    (make-rat (+ (* (numer x) (denom y))
-                 (* (numer y) (denom x)))
-              (* (denom x) (denom y))))
-  (define (sub-rat x y)
-    (make-rat (- (* (numer x) (denom y))
-                 (* (numer y) (denom x)))
-              (* (denom x) (denom y))))
-  (define (mul-rat x y)
-    (make-rat (* (numer x) (numer y))
-              (* (denom x) (denom y))))
-  (define (div-rat x y)
-    (make-rat (* (numer x) (denom y))
-              (* (denom x) (numer y))))
-  (define (equal-complex x y)
-    (and (= (numer x) (numer y))
-         (= (denom x) (denom y))))
-  ;; interface to rest of the system
-  (define (tag x) (attach-tag 'rational x))
-  (put 'add '(rational rational)
-       (lambda (x y) (tag (add-rat x y))))
-  (put 'sub '(rational rational)
-       (lambda (x y) (tag (sub-rat x y))))
-  (put 'mul '(rational rational)
-       (lambda (x y) (tag (mul-rat x y))))
-  (put 'equ? '(rational rational) equal-complex)
-  (put '=zero? '(rational)
-       (lambda (x) (zero? (numer x))))
-  (put 'div '(rational rational)
-       (lambda (x y) (tag (div-rat x y))))
-  (put 'make 'rational
-       (lambda (n d) (tag (make-rat n d))))
-  'done)
-
-(define (make-rational n d)
-  ((get 'make 'rational) n d))
-
-(define (install-complex-package)
-  ;; imported procedures from rectangular and polar packages
-  (define (make-from-real-imag x y)
-    ((get 'make-from-real-imag 'rectangular) x y))
-  (define (make-from-mag-ang r a)
-    ((get 'make-from-mag-ang 'polar) r a))
-  ;; internal procedures
-  (define (add-complex z1 z2)
-    (make-from-real-imag (+ (real-part z1) (real-part z2))
-                         (+ (imag-part z1) (imag-part z2))))
-  (define (sub-complex z1 z2)
-    (make-from-real-imag (- (real-part z1) (real-part z2))
-                         (- (imag-part z1) (imag-part z2))))
-  (define (mul-complex z1 z2)
-    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
-                       (+ (angle z1) (angle z2))))
-  (define (div-complex z1 z2)
-    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
-                       (- (angle z1) (angle z2))))
-  (define (equal-complex z1 z2)
-    (and (= (magnitude z1) (magnitude z2))
-         (= (angle z1) (angle z2))))
-
-  ;; interface to rest of the system
-  (define (tag z) (attach-tag 'complex z))
-  (put 'add '(complex complex)
-       (lambda (z1 z2) (tag (add-complex z1 z2))))
-  (put 'sub '(complex complex)
-       (lambda (z1 z2) (tag (sub-complex z1 z2))))
-  (put 'mul '(complex complex)
-       (lambda (z1 z2) (tag (mul-complex z1 z2))))
-  (put 'div '(complex complex)
-       (lambda (z1 z2) (tag (div-complex z1 z2))))
-  (put 'equ? '(complex complex) equal-complex)
-  (put '=zero? '(complex) 
-       (lambda (z) (zero? (magnitude z))))
-  (put 'make-from-real-imag 'complex
-       (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'complex
-       (lambda (r a) (tag (make-from-mag-ang r a))))
-  (put 'real-part '(complex) real-part)
-  (put 'imag-part '(complex) imag-part)
-  (put 'magnitude '(complex) magnitude)
-  (put 'angle '(complex) angle)
-  'done)
-
-(define (make-complex-from-real-imag x y)
-  ((get 'make-from-real-imag 'complex) x y))
-
-(define (make-complex-from-mag-ang r a)
-  ((get 'make-from-mag-ang 'complex) r a))
-
-(install-rectangular-package)
-(install-polar-package)
-(install-complex-package)
-(install-rational-package)
 (install-scheme-number-package)
 
 ;; Functions to manage coercion
@@ -296,14 +30,33 @@
 (define get-coercion (operation-table 'lookup-proc))
 (define put-coercion (operation-table 'insert-proc!))
 
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags))) 
+      (if proc
+          (apply proc (map contents args)) 
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags)) 
+                    (type2 (cadr type-tags)) 
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types" (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
+
 ;; Louis's coercion procedures
 (define (scheme-number->scheme-number n) n) 
 (define (complex->complex z) z)
-;; Uncomment for a
-;;(put-coercion 'scheme-number
-;;              'scheme-number
-;;              scheme-number->scheme-number)
-;;(put-coercion 'complex 'complex complex->complex)
+(put-coercion 'scheme-number
+              'scheme-number
+              scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
 
 ;; a. What happens?
 (define (exp x y) (apply-generic 'exp x y))
@@ -317,12 +70,36 @@
 ;; complex->complex coercion in an infinite loop
 
 ;; b. Is Louis correct? no because apply-generic works correctly without them
-;; Some examples
+;; But first reset the coercion table
+(define coercion-table (make-table))
 
 (add z1 z2)
 (add 3 4)
 
-;; C.
+;; C. modified apply-generic
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags))) 
+      (if proc
+          (apply proc (map contents args)) 
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags)) 
+                    (type2 (cadr type-tags)))
+                (if (eq? type1 type2)
+                    (error "No method for these types" (list op type-tags))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1))
+                           (a1 (car args))
+                           (a2 (cadr args)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else (error "No method for these types" (list op type-tags)))))))
+                (error "No method for these types"
+                       (list op type-tags)))))))
+
+;; Give it a try
 (exp z1 z2)
 
 
