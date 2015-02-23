6be307cda97d1810a89218a8f9ca24c18fa41f25
Shivram Khandeparker <shivramk@gmail.com>
2015-02-23 23:37:51 +0530

    Moved common code provided by SICP authors into ch2support.scm

--- Chapter2/E2.77.scm
+++ Chapter2/E2.77.scm
@@ -1,45 +1,4 @@
-#lang planet neil/sicp
-
-;; Support for put and get (provided by SICP authors in ch2support.scm)
-;; http://mitpress.mit.edu/sicp/code/ch2support.scm
-(define (assoc key records)
-  (cond ((null? records) false)
-        ((equal? key (caar records)) (car records))
-        (else (assoc key (cdr records)))))
-
-(define (make-table)
-  (let ((local-table (list '*table*)))
-    (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (cdr record)
-                  false))
-            false)))
-    (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (cdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (cdr subtable))))
-              (if record
-                  (set-cdr! record value)
-                  (set-cdr! subtable
-                            (cons (cons key-2 value)
-                                  (cdr subtable)))))
-            (set-cdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (cdr local-table)))))
-      'ok)    
-    (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
-            (else (error "Unknown operation -- TABLE" m))))
-    dispatch))
-
-(define operation-table (make-table))
-(define get (operation-table 'lookup-proc))
-(define put (operation-table 'insert-proc!))
+(include "ch2support.scm")
 
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
