89f7903c04c9e47050bdbaa69c6e2819a2f598f2
Shivram Khandeparker <shivramk@gmail.com>
2015-03-25 23:11:55 +0530

    Better formatting

--- Chapter1/E1.12.scm
+++ Chapter1/E1.12.scm
@@ -1,18 +1,16 @@
-; r, c is row and column
-; indexing starts from 1
+;; `r`, `c` is row and column. Indexing starts from 1
 
 (define (pascal r c)
   (cond ((= c 1) 1)
         ((= c r) 1)
         (else (+ (pascal (- r 1) (- c 1)) (pascal (- r 1) c)))))
 
-;
-; > (pascal 1 1)
-; 1
-; > (pascal 3 2)
-; 2
-; > (pascal 4 2)
-; 3
-; > (pascal 5 3)
-; 6
-; > 
+;;     > (pascal 1 1)
+;;     1
+;;     > (pascal 3 2)
+;;     2
+;;     > (pascal 4 2)
+;;     3
+;;     > (pascal 5 3)
+;;     6
+;;     > 
